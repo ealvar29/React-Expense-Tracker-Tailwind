b5fcb755976a6a99b80740e3d83e7ff5e1ac175e
Ed <edalvarez2015@gmail.com>
2021-06-07 16:38:30 -0500

    TailwindCss added - config h3 style

---
+++ expense-tracker-react/src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Balance() {
+  return (
+    <>
+      <h4 className="">Your Balance</h4>
+      <h1 className="" id="balance">
+        $0.00
+      </h1>
+    </>
+  );
+}
